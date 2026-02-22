aa3a55e5975bbc4822cc363edd9ad26961843535
NazmullSyntax <nazmullsiddik17@gmail.com>
2026-02-23 02:18:33 +0600

    variavle add

--- ml2.py
+++ ml2.py
@@ -1,2 +1,16 @@
-x=4
+x = 4
+x = 'nazmul'
 print(x)
+#variable can be reassign with different data type
+y = 5
+y = 3.14
+print(y)
+#varaiavle conversion
+a = 10
+b = 2.5
+c = a + b
+print(c)
+#variable naming rules
+#1. variable name must start with a letter or underscore
+#2. variable name can contain letters, numbers, and underscores
+#3. variable name cannot start with a number
