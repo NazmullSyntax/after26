78132faa9610cc7c75c42b7ededde086014ee1e6
NazmullSyntax <nazmullsiddik17@gmail.com>
2026-02-23 02:34:58 +0600

    add

--- ml2.py
+++ ml2.py
@@ -1,16 +1,18 @@
-x = 4
-x = 'nazmul'
-print(x)
-#variable can be reassign with different data type
-y = 5
-y = 3.14
-print(y)
-#varaiavle conversion
-a = 10
-b = 2.5
-c = a + b
-print(c)
-#variable naming rules
-#1. variable name must start with a letter or underscore
-#2. variable name can contain letters, numbers, and underscores
-#3. variable name cannot start with a number
+# x = 4
+# x = 'nazmul'
+# print(x)
+# #variable can be reassign with different data type
+# y = 5
+# y = 3.14
+# print(y)
+# #varaiavle conversion
+# a = 10
+# b = 2.5
+# c = a + b
+# print(c)
+# #variable naming rules
+# #1. variable name must start with a letter or underscore
+# #2. variable name can contain letters, numbers, and underscores
+# #3. variable name cannot start with a number
+import random
+print(random.randint(1, 10))
