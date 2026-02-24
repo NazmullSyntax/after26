cf14da507044ff7eaa68f54a33f102b03414b3a0
NazmullSyntax <nazmullsiddik17@gmail.com>
2026-02-25 02:21:19 +0600

    add

--- cd.py
+++ cd.py
@@ -4,6 +4,29 @@
 # # some math
 result = 10+20/5
 print(int(result))
+# vagses
+val =100//3
+print(val)
+#also
+dim =10000%22
+print(dim)
+# some problem more
+size =[10,20,30,40]
+small =min(size)
+print(small)
+big =max(size)
+print(big)
+#power
+base = 5
+exponent = 3
+power = base ** exponent
+print(power)
+#next
+base =10
+exponent = 2
+result = pow(base, exponent)
+print(result)
+
 
 
 
